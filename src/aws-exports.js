12d155da9c4e87645478762bc542a2de26ec54bc
Kelsey <pages.kelsey@gmail.com>
2022-02-09 14:38:18 -0500

    create react app

---
+++ src/aws-exports.js
@@ -0,0 +1,11 @@
+const awscognito = {
+  "aws_project_region": "us-east-1",
+  "aws_cognito_identity_pool_id": "", //identity pool id from Cognito
+  "aws_cognito_region": "us-east-1",
+  "aws_user_pools_id": "", //users pool id from Cognito
+  "aws_user_pools_web_client_id": "", //App client id from the user pool from Cognito
+  "oauth": {}
+};
+
+
+export default awscognito;
